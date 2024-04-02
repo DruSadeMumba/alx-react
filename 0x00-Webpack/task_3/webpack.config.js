8fac3c2570804d7d897ee3b6f4fbf7ee5e53ce2a
Dru Mumba <drusademumba@gmail.com>
2024-04-02 20:24:00 +0300

    3. Dev servers, modules, and tree shaking

--- 0x00-Webpack/task_3/webpack.config.js
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -1,14 +1,30 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  plugins: [
+    new HTMLWebpackPlugin({
+      filename: './index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
   devtool: 'inline-source-map',
+  mode: 'development',
   entry: {
-    header: './modules/header/header.js',
-    body: './modules/body/body.js',
-    footer: './modules/footer/footer.js',
+    header: {
+      import: './modules/header/header.js',
+      dependOn: 'shared',
+    },
+    body: {
+      import: './modules/body/body.js',
+      dependOn: 'shared',
+    },
+    footer: {
+      import: './modules/footer/footer.js',
+      dependOn: 'shared',
+    },
+    shared: 'jquery',
   },
   output: {
     path: path.resolve(__dirname, 'public'),
@@ -19,44 +35,34 @@ module.exports = {
       chunks: 'all',
     },
   },
+  devServer: {
+    static: path.join(__dirname, './public'),
+    open: true,
+    port: 8564,
+  },
   performance: {
-    hints: false,
-    maxEntrypointSize: 512000,
-    maxAssetSize: 512000,
+    maxAssetSize: 1000000,
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ["css-loader", "style-loader"],
       },
       {
-        test: /\.(gif|png|jp?g|svg)$/i,
+        test: /\.(?:ico|gif|png|jpe?g|svg)$/i,
+        type: 'asset/resource',
         use: [
           "file-loader",
           {
             loader: "image-webpack-loader",
             options: {
-              bypassOnDebug: true,
+              bypassingOnDebug: true,
               disable: true,
-            }
+            },
           },
         ],
       },
-    ]
+    ],
   },
-  devServer: {
-    port: 8564,
-    static: {
-      directory: path.join(__dirname, 'public'),
-    },
-    compress: true,
-    open: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: './index.html',
-    }),
-    new CleanWebpackPlugin(),
-  ],
 };

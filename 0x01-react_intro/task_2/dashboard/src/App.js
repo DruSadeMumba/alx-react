8b27250639ff72a030ef48744979d401559d01e3
Dru Mumba <drusademumba@gmail.com>
2024-04-03 15:34:49 +0300

    4. Modify the Notifications

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -13,14 +13,16 @@ function App() {
       <hr/>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
-        <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" />
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" />
-        <button type="submit">OK</button>
+        <div className="login">
+          <label htmlFor="email">Email: </label>
+          <input type="email" name="email" id="email" />
+          <label htmlFor="password">Password: </label>
+          <input type="password" name="password" id="password" />
+          <button type="submit">OK</button>
+        </div>
       </div>
-      <hr/>
       <footer className="App-footer">
+        <hr/>
         <p><em>Copyright {getFullYear()} - {getFooterCopy()}</em></p>
       </footer>
     </div>

7f3118d6b41499ac5e2ec89fc1d81002773e1ab2
Dru Mumba <drusademumba@gmail.com>
2024-04-06 19:27:57 +0300

    1. Write the tests for each component

---
+++ 0x02-react_props/task_1/dashboard/src/Footer/Footer.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Footer from './Footer';
+
+configure({ adapter: new Adapter() });
+describe('Footer component', () => {
+  it('renders without crashing', () => {
+    shallow(<Footer />);
+  });
+
+  it('renders the text Copyright', () => {
+    const wrapper = shallow(<Footer />);
+    const text = wrapper.find('p').text();
+    expect(text).toEqual('Copyright 2024 - Holberton School main dashboards');
+  });
+});

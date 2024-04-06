ef4cfefd5126ebe55f25cdfe72e61515dceeede1
Dru Mumba <drusademumba@gmail.com>
2024-04-06 19:26:25 +0300

    Move files

--- 0x02-react_props/task_0/dashboard/src/App/App.test.js
+++ 0x02-react_props/task_0/dashboard/src/App/App.test.js
@@ -4,7 +4,6 @@ import { shallow, configure } from 'enzyme';
 import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
-
 test('renders App without crashing', () => {
   const wrapper = shallow(<App />);
   expect(wrapper.exists()).toBeTruthy();

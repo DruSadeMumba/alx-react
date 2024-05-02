2503c197c5cafd436e6cb015562cf750436c093a
Dru Mumba <drusademumba@gmail.com>
2024-05-02 17:05:00 +0300

    7. Async Action Creators

--- 0x07-react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.js
+++ 0x07-react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.js
@@ -1,8 +1,17 @@
-import { LOGIN, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER } from './uiActionTypes';
+import { LOGIN, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER, LOGIN_SUCCESS, LOGIN_FAILURE } from './uiActionTypes';
+import fetch from 'node-fetch';
 
 export const login = (email, password) => ({
   type: LOGIN,
   user: { email, password }
+});
+
+export const loginSuccess = () => ({
+  type: LOGIN_SUCCESS
+});
+
+export const loginFailure = () => ({
+  type: LOGIN_FAILURE
 });
 
 export const logout = () => ({
@@ -16,6 +25,22 @@ export const displayNotificationDrawer = () => ({
 export const hideNotificationDrawer = () => ({
   type: HIDE_NOTIFICATION_DRAWER
 });
+
+export const loginRequest = (email, password) => (dispatch) => {
+  boundLogin(email, password);
+  let url = '../../dist/login-success.json';
+  fetch(url)
+    .then(res => {
+      if (res.ok) {
+        dispatch(loginSuccess());
+      } else {
+        dispatch(loginFailure());
+      }
+    })
+    .catch(() => {
+      dispatch(loginFailure());
+    });
+};
 
 export const boundLogin = (email, password) => dispatch(login(email, password));
 export const boundLogout = () => dispatch(logout);

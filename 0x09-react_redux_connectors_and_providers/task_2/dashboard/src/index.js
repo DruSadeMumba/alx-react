dca43d91350bd7b60e3cbc27fca964dd4c995d6e
Dru Mumba <drusademumba@gmail.com>
2024-05-12 20:33:24 +0300

    7. Async actions & Thunk middleware

--- 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./App/App";
-import { uiReducer } from './reducers/uiReducer';
-import { createStore } from 'redux';
+import { uiReducer, initialState } from './reducers/uiReducer';
+import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { Map } from 'immutable';
+import { thunk } from 'redux-thunk';
 
-const store = createStore(uiReducer);
+const store = createStore(uiReducer, Map(initialState), applyMiddleware(thunk));
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>

76af4dcd99267c10326b469f0d773d872a19815e
Dru Mumba <drusademumba@gmail.com>
2024-05-12 20:24:01 +0300

    6. Update your tests

--- 0x09-react_redux_connectors_and_providers/task_1/dashboard/src/App/App.test.js
+++ 0x09-react_redux_connectors_and_providers/task_1/dashboard/src/App/App.test.js
@@ -8,10 +8,24 @@ configure({ adapter: new Adapter() });
 StyleSheetTestUtils.suppressStyleInjection();
 
 describe('App component', () => {
-  it('returns the correct object when passing the state', () => {
-    let state = fromJS({
-      isUserLoggedIn: true
+  describe('mapStateToProps returns the correct object when passing the state', () => {
+    it('returns correct object from user Login', () => {
+      let state = fromJS({
+        isUserLoggedIn: true
+      });
+      expect(mapStateToProps(state)).toEqual({ isLoggedIn: true });
     });
-    expect(mapStateToProps(state)).toEqual({ isLoggedIn: true });
+    it('returns correct object from user logout', () => {
+      let state = fromJS({
+        isUserLoggedIn: false
+      });
+      expect(mapStateToProps(state)).toEqual({ isLoggedIn: false });
+    });
+    it('returns correct object from display Drawer', () => {
+      let state = fromJS({
+        isNotificationDrawerVisible: true
+      });
+      expect(mapStateToProps(state)).toEqual({ displayDrawer: true });
+    });
   });
 });

a65fb0581fbccd6566e249c8f6a9084bf080fce1
Dru Mumba <drusademumba@gmail.com>
2024-05-14 16:14:51 +0300

    13. Understand how to use the Redux Chrome extension

--- 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./App/App";
 import { uiReducer, initialState } from './reducers/uiReducer';
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
 import { Provider } from 'react-redux';
 import { Map } from 'immutable';
 import { thunk } from 'redux-thunk';
 
-const store = createStore(uiReducer, Map(initialState), applyMiddleware(thunk));
+const composeWithDevTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(uiReducer, Map(initialState), composeWithDevTools(applyMiddleware(thunk)));
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>

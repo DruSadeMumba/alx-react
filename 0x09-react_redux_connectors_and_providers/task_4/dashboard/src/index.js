6e6a03b678abc3b0edbd01f84de216de0dcf1d37
Dru Mumba <drusademumba@gmail.com>
2024-05-14 16:26:43 +0300

    Add files

--- 0x09-react_redux_connectors_and_providers/task_4/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_4/dashboard/src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./App/App";
+import { uiReducer, initialState } from './reducers/uiReducer';
+import { applyMiddleware, createStore, compose } from 'redux';
+import { Provider } from 'react-redux';
+import { Map } from 'immutable';
+import { thunk } from 'redux-thunk';
+
+const composeWithDevTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(uiReducer, Map(initialState), composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
